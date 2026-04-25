9e0716350838ea7f1d597b52688f7a156d69b00e
sacilingir <seyitacilingir@gmail.com>
2026-04-25 20:29:37 +0300

    Entity configuration classları oluşturuldu

--- Core/PazarApi.Domain/Entities/Product.cs
+++ Core/PazarApi.Domain/Entities/Product.cs
@@ -8,6 +8,7 @@ namespace PazarApi.Domain.Entities
         public required string Title { get; set; }
         public required string Description { get; set; }
         public required int BrandId { get; set; }
+        public required decimal Price { get; set; }
         public Brand Brand { get; set; }
         public required decimal Discount { get; set; }
         public ICollection<Category> Categories { get; set; }
